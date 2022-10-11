a4e4f58b40a970ec34b69c283216b5379fc0414d
Daniel Macovei <djmacovei@gmail.com>
2022-10-11 15:09:28 -0500

    initial commit

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module http-interfaces
+
+go 1.17
---
+++ go/main.go
@@ -0,0 +1,43 @@
+package main
+
+type Request struct {
+	Method Method
+	Url string
+	Headers []Header
+	Params []Param
+	Body []byte
+}
+
+type Method uint8
+
+type Header [2]string
+
+type Param map[string][]string
+
+const (
+	GET Method = iota
+	POST
+	PUT
+	DELETE
+)
+
+type Response struct {
+	Status int
+	Body   []byte
+}
+
+//go:wasm-module jig
+//go:export httpRequest
+func request(method Method, url string, reqBody []byte) *Response
+
+//go:export allocateResponse
+func allocateResponse(status, length int) *Response {
+	return &Response{status, make([]byte, length)}
+}
+
+//go:export performRequest
+func performRequest(url string) *Response {
+	// GETS DATA, AND WRITES RES TO MEM
+	res := request(POST, url, []byte("something~"))
+	return allocateResponse(res.Status, len(string(res.Body)))
+}
